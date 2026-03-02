d3602289167ffdbb2038c951a5aeae14d226a2e3
Aizhannat <aborubaeva@na.edu>
2026-03-02 10:45:15 -0600

    дуыlesson1

--- lesson1.py
+++ lesson1.py
@@ -5,9 +5,32 @@ class Hero:
         self.health = health
         self.strength = strength
 
-    def action(self):
-        return f'{self.name}base action avtivate!!'
+    def greet(self):
+        return f'Привет {self.name}, мой уровень {self.level}'
+
+    def attack(self):
+        self.strength -= 1
+        return f'{self.name} наносит удар!'
+
+
+    def rest(self):
+        self.health += 1
+        return f'{self.name} отдыхает...'
+
 
 janka = Hero("Janka", 100,1000, 100)
 marvel = Hero("Marvel", 101,1100, 101)
 
+
+print(janka.greet())
+print(janka.attack())
+print(janka.rest())
+print(f'Здоровье: {janka.health}')
+print(f'Сила: {janka.strength}')
+
+
+print(marvel.greet())
+print(marvel.attack())
+print(marvel.rest())
+print(f'Здоровье: {marvel.health}')
+print(f'Сила: {marvel.strength}')
