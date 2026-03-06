30efc287f35ce4dfb50023cc640d301064ef3194
Aizhannat <aborubaeva@na.edu>
2026-03-06 01:46:21 -0600

    lesson2

---
+++ Lessons/lesson2.py
@@ -0,0 +1,69 @@
+"""class Hero:
+    def __init__(self, name, lvl, hp):
+        self.name = name
+        self.lvl = lvl
+        self.hp = hp
+
+    def action(self):
+        return f"{self.name} base action"
+
+class MageHero(Hero):
+
+    def __init__(self, name, lvl, hp, mp):
+        super().__init__(name, lvl, hp)
+        self.mp = mp
+
+    def action(self):
+        return f'HP: {self.hp} NAME: {self.name}'
+
+    def cast_spell(self):
+        return f'{self.mp} cast spell {self.name}'
+
+
+
+
+kirito = Hero("Kirito",100, 1000)
+asuna = MageHero("Asuna",100, 1000, 11)
+
+print(kirito.action())
+print(asuna.action())"""
+
+"""class Fly:
+    def action(self):
+        print("Fly")
+
+
+class Swim:
+    def action(self):
+        print("Swim")
+
+class Animal(Swim, Fly):
+    def action(self):
+        print("Animal action")
+
+duck = Animal()
+duck.action()
+
+print(Animal.mro())"""
+
+
+
+'''class A:
+    def action(self):
+        print("A")
+class B(A):
+    def action(self):
+        print("B")
+class C(A):
+    def action(self):
+        print("C")
+class D(B,C):
+    def action(self):
+        super().action()
+        print("D")
+
+test = D()
+#test.action()
+#print(D.mro())'''
+
+
