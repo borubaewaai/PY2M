f8e3ba93e007490f52bb60917a5a958352061e10
Aizhannat <aborubaeva@na.edu>
2026-03-29 00:10:56 -0500

    hw6

---
+++ HW/hw6.py
@@ -0,0 +1,16 @@
+from faker import Faker
+#эта библиотека нужна для генерации случайных данных
+fake = Faker()
+print(fake.name())
+
+def two_sum(nums, target):
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+
+nums = [2, 7, 11, 15]
+target = 9
+
+result = two_sum(nums, target)
+print(result)
